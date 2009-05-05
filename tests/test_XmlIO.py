46016b1ac71940c980ce3f576144bc88889ec78b
Bobby R. Ward <bobbyrward@gmail.com>
2009-05-05 12:55:53 -0500

    Update xml io tests

--- tests/test_XmlIO.py
+++ tests/test_XmlIO.py
@@ -29,6 +29,8 @@ from testing_util import print_test_name
 test_flam3 = [
 """
 <test>
+""",
+"""
 <flame time="0" palette="15" size="640 480" center="0 0" scale="240" zoom="0" oversample="1" filter="1" quality="10" background="0 0 0" brightness="4" gamma="4" vibrancy="1" hue="0.22851">
    <xform weight="0.25" color="1" spherical="1" coefs="-0.681206 -0.0779465 0.20769 0.755065 -0.0416126 -0.262334"/>
    <xform weight="0.25" color="0.66" spherical="1" coefs="0.953766 0.48396 0.43268 -0.0542476 0.642503 -0.995898"/>
@@ -43,6 +45,8 @@ test_flam3 = [
    <xform weight="0.25" color="0.33" spherical="1" coefs="0.0738451 -0.349212 -0.635205 0.262572 -0.398985 -0.736904"/>
    <xform weight="0.25" color="0" spherical="1" coefs="0.992697 0.433488 -0.427202 -0.339112 -0.507145 0.120765"/>
 </flame>
+""",
+"""
 </test>
 """,
 ]
@@ -50,14 +54,33 @@ test_flam3 = [
 class TestCase(unittest.TestCase):
     @print_test_name
     def testToFromXml(self):
-        genomes = pyflam3ng.flam3_from_xml(''.join(test_flam3))
+        genomes = pyflam3ng.flam3.from_xml(''.join(test_flam3))
         self.assertEqual(len(genomes), 2)
 
-        xml = pyflam3ng.flam3_to_xml(genomes[0])
+        xml = pyflam3ng.flam3.to_xml(genomes[0])
         self.assertTrue(xml)
 
-        xml = pyflam3ng.flam3_to_xml(genomes[0])
+        xml = pyflam3ng.flam3.to_xml(genomes[0])
         self.assertTrue(xml)
+
+    def _check_genome_1(self, test):
+        pass
+
+    def _check_genome_2(self, test):
+        pass
+
+    @print_test_name
+    def testHighlevelIO(self):
+        gene1, gene2 = pyflam3ng.load_flame(xml_source=''.join(test_flam3))
+        self._check_genome_1(gene1)
+        self._check_genome_2(gene2)
+
+        gene1 = pyflam3ng.load_genome(xml_source=test_flam3[1])
+        self._check_genome_1(gene1)
+
+        gene2 = pyflam3ng.load_genome(xml_source=test_flam3[2])
+        self._check_genome_1(gene2)
+
 
 
 

733f0393ee4e53281a4d9009f63f90b6774ad1dc
Bobby R. Ward <bobbyrward@gmail.com>
2009-05-07 05:46:30 -0500

    Refactor variation code

--- pyflam3ng/Variants.py
+++ pyflam3ng/Variants.py
@@ -1,525 +1,106 @@
 
-class Var_Linear(Object):
-	def __init__(self, value=1.0):
-		self.name = 'linear'
-		self.value = value
 
-
-class Var_Sinusoidal(Object):
-	def __init__(self, value=1.0):
-		self.name = 'sinusoidal'
-		self.value = value
-
-
-class Var_Spherical(Object):
-	def __init__(self, value=1.0):
-		self.name = 'spherical'
-		self.value = value
-
-
-class Var_Swirl(Object):
-	def __init__(self, value=1.0):
-		self.name = 'swirl'
-		self.value = value
-
-
-class Var_Horseshoe(Object):
-	def __init__(self, value=1.0):
-		self.name = 'horseshoe'
-		self.value = value
-
-
-class Var_Polar(Object):
-	def __init__(self, value=1.0):
-		self.name = 'polar'
-		self.value = value
-
-
-class Var_Handkerchief(Object):
-	def __init__(self, value=1.0):
-		self.name = 'handkerchief'
-		self.value = value
-
-
-class Var_Heart(Object):
-	def __init__(self, value=1.0):
-		self.name = 'heart'
-		self.value = value
-
-
-class Var_Disc(Object):
-	def __init__(self, value=1.0):
-		self.name = 'disc'
-		self.value = value
-
-
-class Var_Spiral(Object):
-	def __init__(self, value=1.0):
-		self.name = 'spiral'
-		self.value = value
-
-
-class Var_Hyperbolic(Object):
-	def __init__(self, value=1.0):
-		self.name = 'hyperbolic'
-		self.value = value
-
-
-class Var_Diamond(Object):
-	def __init__(self, value=1.0):
-		self.name = 'diamond'
-		self.value = value
-
-
-class Var_Ex(Object):
-	def __init__(self, value=1.0):
-		self.name = 'ex'
-		self.value = value
-
-
-class Var_Julia(Object):
-	def __init__(self, value=1.0):
-		self.name = 'julia'
-		self.value = value
-
-
-class Var_Bent(Object):
-	def __init__(self, value=1.0):
-		self.name = 'bent'
-		self.value = value
-
-
-class Var_Waves(Object):
-	def __init__(self, value=1.0):
-		self.name = 'waves'
-		self.value = value
-
-
-class Var_Fisheye(Object):
-	def __init__(self, value=1.0):
-		self.name = 'fisheye'
-		self.value = value
-
-
-class Var_Popcorn(Object):
-	def __init__(self, value=1.0):
-		self.name = 'popcorn'
-		self.value = value
-
-
-class Var_Exponential(Object):
-	def __init__(self, value=1.0):
-		self.name = 'exponential'
-		self.value = value
-
-
-class Var_Power(Object):
-	def __init__(self, value=1.0):
-		self.name = 'power'
-		self.value = value
-
-
-class Var_Cosine(Object):
-	def __init__(self, value=1.0):
-		self.name = 'cosine'
-		self.value = value
-
-
-class Var_Rings(Object):
-	def __init__(self, value=1.0):
-		self.name = 'rings'
-		self.value = value
-
-
-class Var_Fan(Object):
-	def __init__(self, value=1.0):
-		self.name = 'fan'
-		self.value = value
-
-
-class Var_Blob(Object):
-	def __init__(self, value=1.0, variables={'high':1, 'low':0, 'waves':1, }):
-		self.name = 'blob'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Pdj(Object):
-	def __init__(self, value=1.0, variables={'a':0, 'c':0, 'b':0, 'd':0, }):
-		self.name = 'pdj'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Fan2(Object):
-	def __init__(self, value=1.0, variables={'y':1, 'x':0, }):
-		self.name = 'fan2'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Rings2(Object):
-	def __init__(self, value=1.0, variables={'val':0, }):
-		self.name = 'rings2'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Eyefish(Object):
-	def __init__(self, value=1.0):
-		self.name = 'eyefish'
-		self.value = value
-
-
-class Var_Bubble(Object):
-	def __init__(self, value=1.0):
-		self.name = 'bubble'
-		self.value = value
-
-
-class Var_Cylinder(Object):
-	def __init__(self, value=1.0):
-		self.name = 'cylinder'
-		self.value = value
-
-
-class Var_Perspective(Object):
-	def __init__(self, value=1.0, variables={'dist':0, 'angle':0, }):
-		self.name = 'perspective'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Noise(Object):
-	def __init__(self, value=1.0):
-		self.name = 'noise'
-		self.value = value
-
-
-class Var_Julian(Object):
-	def __init__(self, value=1.0, variables={'dist':1, 'power':1, }):
-		self.name = 'julian'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Juliascope(Object):
-	def __init__(self, value=1.0, variables={'dist':1, 'power':1, }):
-		self.name = 'juliascope'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Blur(Object):
-	def __init__(self, value=1.0):
-		self.name = 'blur'
-		self.value = value
-
-
-class Var_Gaussian_Blur(Object):
-	def __init__(self, value=1.0):
-		self.name = 'gaussian_blur'
-		self.value = value
-
-
-class Var_Radial_Blur(Object):
-	def __init__(self, value=1.0):
-		self.name = 'radial_blur'
-		self.value = value
-
-
-class Var_Pie(Object):
-	def __init__(self, value=1.0, variables={'rotation':0, 'thickness':0, 'slices':6, }):
-		self.name = 'pie'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Ngon(Object):
-	def __init__(self, value=1.0, variables={'corners':2, 'circle':1, 'sides':5, 'power':3, }):
-		self.name = 'ngon'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Curl(Object):
-	def __init__(self, value=1.0, variables={'c2':0, 'c1':1, }):
-		self.name = 'curl'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Rectangles(Object):
-	def __init__(self, value=1.0, variables={'y':1, 'x':1, }):
-		self.name = 'rectangles'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Arch(Object):
-	def __init__(self, value=1.0):
-		self.name = 'arch'
-		self.value = value
-
-
-class Var_Tangent(Object):
-	def __init__(self, value=1.0):
-		self.name = 'tangent'
-		self.value = value
-
-
-class Var_Square(Object):
-	def __init__(self, value=1.0):
-		self.name = 'square'
-		self.value = value
-
-
-class Var_Rays(Object):
-	def __init__(self, value=1.0):
-		self.name = 'rays'
-		self.value = value
-
-
-class Var_Blade(Object):
-	def __init__(self, value=1.0):
-		self.name = 'blade'
-		self.value = value
-
-
-class Var_Secant2(Object):
-	def __init__(self, value=1.0):
-		self.name = 'secant2'
-		self.value = value
-
-
-class Var_Twintrian(Object):
-	def __init__(self, value=1.0):
-		self.name = 'twintrian'
-		self.value = value
-
-
-class Var_Cross(Object):
-	def __init__(self, value=1.0):
-		self.name = 'cross'
-		self.value = value
-
-
-class Var_Disc2(Object):
-	def __init__(self, value=1.0, variables={'twist':0, 'rot':0, }):
-		self.name = 'disc2'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Super_Shape(Object):
-	def __init__(self, value=1.0):
-		self.name = 'super_shape'
-		self.value = value
-
-
-class Var_Flower(Object):
-	def __init__(self, value=1.0, variables={'petals':0, 'holes':0, }):
-		self.name = 'flower'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Conic(Object):
-	def __init__(self, value=1.0, variables={'eccentricity':1, 'holes':0, }):
-		self.name = 'conic'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Parabola(Object):
-	def __init__(self, value=1.0, variables={'width':0, 'height':0, }):
-		self.name = 'parabola'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Bent2(Object):
-	def __init__(self, value=1.0, variables={'y':0, 'x':0, }):
-		self.name = 'bent2'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Bipolar(Object):
-	def __init__(self, value=1.0, variables={'shift':0, }):
-		self.name = 'bipolar'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Boarders(Object):
-	def __init__(self, value=1.0):
-		self.name = 'boarders'
-		self.value = value
-
-
-class Var_Butterfly(Object):
-	def __init__(self, value=1.0):
-		self.name = 'butterfly'
-		self.value = value
-
-
-class Var_Cell(Object):
-	def __init__(self, value=1.0, variables={'size':1, }):
-		self.name = 'cell'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Cpow(Object):
-	def __init__(self, value=1.0, variables={'i':0, 'r':1, 'power':1, }):
-		self.name = 'cpow'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Curve(Object):
-	def __init__(self, value=1.0, variables={'ylength':1, 'xlength':1, 'yamp':1, 'xamp':1, }):
-		self.name = 'curve'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Edisc(Object):
-	def __init__(self, value=1.0):
-		self.name = 'edisc'
-		self.value = value
-
-
-class Var_Elliptic(Object):
-	def __init__(self, value=1.0):
-		self.name = 'elliptic'
-		self.value = value
-
-
-class Var_Escher(Object):
-	def __init__(self, value=1.0, variables={'beta':0, }):
-		self.name = 'escher'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Foci(Object):
-	def __init__(self, value=1.0):
-		self.name = 'foci'
-		self.value = value
-
-
-class Var_Lazysusan(Object):
-	def __init__(self, value=1.0, variables={'twist':0, 'x':1, 'spin':0, 'y':1, 'space':0, }):
-		self.name = 'lazysusan'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Loonie(Object):
-	def __init__(self, value=1.0):
-		self.name = 'loonie'
-		self.value = value
-
-
-class Var_Pre_Blur(Object):
-	def __init__(self, value=1.0):
-		self.name = 'pre_blur'
-		self.value = value
-
-
-class Var_Modulus(Object):
-	def __init__(self, value=1.0, variables={'y':1, 'x':1, }):
-		self.name = 'modulus'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Oscilloscope(Object):
-	def __init__(self, value=1.0):
-		self.name = 'oscilloscope'
-		self.value = value
-
-
-class Var_Polar2(Object):
-	def __init__(self, value=1.0):
-		self.name = 'polar2'
-		self.value = value
-
-
-class Var_Popcorn2(Object):
-	def __init__(self, value=1.0, variables={'y':0, 'x':0, 'c':0, }):
-		self.name = 'popcorn2'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Scry(Object):
-	def __init__(self, value=1.0):
-		self.name = 'scry'
-		self.value = value
-
-
-class Var_Separation(Object):
-	def __init__(self, value=1.0, variables={'y':0, 'x':0, 'xinside':0, 'yinside':0, }):
-		self.name = 'separation'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Split(Object):
-	def __init__(self, value=1.0, variables={'ysize':0, 'xsize':0, }):
-		self.name = 'split'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Splits(Object):
-	def __init__(self, value=1.0, variables={'y':1, 'x':1, }):
-		self.name = 'splits'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Stripes(Object):
-	def __init__(self, value=1.0, variables={'warp':0, 'space':1, }):
-		self.name = 'stripes'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Wedge(Object):
-	def __init__(self, value=1.0, variables={'count':0, 'swirl':0, 'hole':0, 'angle':0, }):
-		self.name = 'wedge'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Wedge_Julia(Object):
-	def __init__(self, value=1.0, variables={'count':0, 'dist':1, 'angle':0, 'power':2, }):
-		self.name = 'wedge_julia'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Wedge_Sph(Object):
-	def __init__(self, value=1.0, variables={'count':0, 'swirl':0, 'hole':0, 'angle':0, }):
-		self.name = 'wedge_sph'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Whorl(Object):
-	def __init__(self, value=1.0, variables={'inside':1, 'outside':1, }):
-		self.name = 'whorl'
-		self.value = value
-		self.variables = variables
-
-
-class Var_Waves2(Object):
-	def __init__(self, value=1.0, variables={'scalex':1, 'scaley':1, 'freqy':1, 'freqx':1, }):
-		self.name = 'waves2'
-		self.value = value
-		self.variables = variables
+variation_registry = {}
+
+def define_variation(class_name, xml_name, variables=None):
+    class Var(object):
+        def __init__(self, value=1.0, **kwargs):
+            self.value = value
+
+            if variables is not None:
+                for var_name, default in variables.iteritems():
+                    setattr(self, var_name, kwargs.get(var_name, default))
+
+        def name(self):
+            return xml_name
+
+    c = Var
+    c.__name__ = class_name
+
+    variation_registry[xml_name] = c
+
+    return c
+
+Linear = define_variation('Linear', 'linear')
+Sinusoidal = define_variation('Sinusoidal', 'sinusoidal')
+Spherical = define_variation('Spherical', 'spherical')
+Swirl = define_variation('Swirl', 'swirl')
+Horseshoe = define_variation('Horseshoe', 'horseshoe')
+Polar = define_variation('Polar', 'polar')
+Handkerchief = define_variation('Handkerchief', 'handkerchief')
+Heart = define_variation('Heart', 'heart')
+Disc = define_variation('Disc', 'disc')
+Spiral = define_variation('Spiral', 'spiral')
+Hyperbolic = define_variation('Hyperbolic', 'hyperbolic')
+Diamond = define_variation('Diamond', 'diamond')
+Ex = define_variation('Ex', 'ex')
+Julia = define_variation('Julia', 'julia')
+Bent = define_variation('Bent', 'bent')
+Waves = define_variation('Waves', 'waves')
+Fisheye = define_variation('Fisheye', 'fisheye')
+Popcorn = define_variation('Popcorn', 'popcorn')
+Exponential = define_variation('Exponential', 'exponential')
+Power = define_variation('Power', 'power')
+Cosine = define_variation('Cosine', 'cosine')
+Rings = define_variation('Rings', 'rings')
+Fan = define_variation('Fan', 'fan')
+Blob = define_variation('Blob', 'blob', {'high':1, 'low':0, 'waves':1, })
+Pdj = define_variation('Pdj', 'pdj', {'a':0, 'c':0, 'b':0, 'd':0, })
+Fan2 = define_variation('Fan2', 'fan2', {'y':1, 'x':0, })
+Rings2 = define_variation('Rings2', 'rings2', {'val':0, })
+Eyefish = define_variation('Eyefish', 'eyefish')
+Bubble = define_variation('Bubble', 'bubble')
+Cylinder = define_variation('Cylinder', 'cylinder')
+Perspective = define_variation('Perspective', 'perspective', {'dist':0, 'angle':0, })
+Noise = define_variation('Noise', 'noise')
+Julian = define_variation('Julian', 'julian', {'dist':1, 'power':1, })
+Juliascope = define_variation('Juliascope', 'juliascope', {'dist':1, 'power':1, })
+Blur = define_variation('Blur', 'blur')
+Gaussian_Blur = define_variation('Gaussian_Blur', 'gaussian_blur')
+Radial_Blur = define_variation('Radial_Blur', 'radial_blur')
+Pie = define_variation('Pie', 'pie', {'rotation':0, 'thickness':0, 'slices':6, })
+Ngon = define_variation('Ngon', 'ngon', {'corners':2, 'circle':1, 'sides':5, 'power':3, })
+Curl = define_variation('Curl', 'curl', {'c2':0, 'c1':1, })
+Rectangles = define_variation('Rectangles', 'rectangles', {'y':1, 'x':1, })
+Arch = define_variation('Arch', 'arch')
+Tangent = define_variation('Tangent', 'tangent')
+Square = define_variation('Square', 'square')
+Rays = define_variation('Rays', 'rays')
+Blade = define_variation('Blade', 'blade')
+Secant2 = define_variation('Secant2', 'secant2')
+Twintrian = define_variation('Twintrian', 'twintrian')
+Cross = define_variation('Cross', 'cross')
+Disc2 = define_variation('Disc2', 'disc2', {'twist':0, 'rot':0, })
+Super_Shape = define_variation('Super_Shape', 'super_shape')
+Flower = define_variation('Flower', 'flower', {'petals':0, 'holes':0, })
+Conic = define_variation('Conic', 'conic', {'eccentricity':1, 'holes':0, })
+Parabola = define_variation('Parabola', 'parabola', {'width':0, 'height':0, })
+Bent2 = define_variation('Bent2', 'bent2', {'y':0, 'x':0, })
+Bipolar = define_variation('Bipolar', 'bipolar', {'shift':0, })
+Boarders = define_variation('Boarders', 'boarders')
+Butterfly = define_variation('Butterfly', 'butterfly')
+Cell = define_variation('Cell', 'cell', {'size':1, })
+Cpow = define_variation('Cpow', 'cpow', {'i':0, 'r':1, 'power':1, })
+Curve = define_variation('Curve', 'curve', {'ylength':1, 'xlength':1, 'yamp':1, 'xamp':1, })
+Edisc = define_variation('Edisc', 'edisc')
+Elliptic = define_variation('Elliptic', 'elliptic')
+Escher = define_variation('Escher', 'escher', {'beta':0, })
+Foci = define_variation('Foci', 'foci')
+Lazysusan = define_variation('Lazysusan', 'lazysusan', {'twist':0, 'x':1, 'spin':0, 'y':1, 'space':0, })
+Loonie = define_variation('Loonie', 'loonie')
+Pre_Blur = define_variation('Pre_Blur', 'pre_blur')
+Modulus = define_variation('Modulus', 'modulus', {'y':1, 'x':1, })
+Oscilloscope = define_variation('Oscilloscope', 'oscilloscope')
+Polar2 = define_variation('Polar2', 'polar2')
+Popcorn2 = define_variation('Popcorn2', 'popcorn2', {'y':0, 'x':0, 'c':0, })
+Scry = define_variation('Scry', 'scry')
+Separation = define_variation('Separation', 'separation', {'y':0, 'x':0, 'xinside':0, 'yinside':0, })
+Split = define_variation('Split', 'split', {'ysize':0, 'xsize':0, })
+Splits = define_variation('Splits', 'splits', {'y':1, 'x':1, })
+Stripes = define_variation('Stripes', 'stripes', {'warp':0, 'space':1, })
+Wedge = define_variation('Wedge', 'wedge', {'count':0, 'swirl':0, 'hole':0, 'angle':0, })
+Wedge_Julia = define_variation('Wedge_Julia', 'wedge_julia', {'count':0, 'dist':1, 'angle':0, 'power':2, })
+Wedge_Sph = define_variation('Wedge_Sph', 'wedge_sph', {'count':0, 'swirl':0, 'hole':0, 'angle':0, })
+Whorl = define_variation('Whorl', 'whorl', {'inside':1, 'outside':1, })
+Waves2 = define_variation('Waves2', 'waves2', {'scalex':1, 'scaley':1, 'freqy':1, 'freqx':1, })
 

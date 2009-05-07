b648be9857d09bcaaa18fde15fcd0630e1d10db9
John Miller <john.jj.miller@gmail.com>
2009-05-06 21:33:09 -0400

    Add Xforms, polar functions for Point.

---
+++ pyflam3ng/func.py
@@ -0,0 +1,12 @@
+from math import *
+
+def polar(x, y):
+    len = sqrt(x**2 + y**2)
+    ang = atan2(y, x) * (180.0/pi)
+    return len, ang
+
+def rect(len, ang):
+    x = len * cos(ang*pi/180.0)
+    y = len * sin(ang*pi/180.0)
+    return real, imag
+

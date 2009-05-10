43128f11cbaba300b5634519c1fe8a7c60750516
macbony <john.jj.miller@gmail.com>
2009-05-10 13:17:39 -0400

    Moved all vutils to numpy calcs

--- pyflam3ng/vector_utils.py
+++ pyflam3ng/vector_utils.py
@@ -9,7 +9,7 @@ for animation and interpolation.
 import numpy, math
 
 
-def crange(x, y, n, curve='lin', a=1.0, b=0.5, c=1.0):
+def crange(x, y, n, curve='lin', p1=1.0, p2=0.5, p3=1.0):
     """Generates a curved range between x and y with n points.
 
     Arguments:
@@ -17,22 +17,26 @@ def crange(x, y, n, curve='lin', a=1.0, b=0.5, c=1.0):
         y       - Ending value
         n       - Number of steps from x to y
         curve   - Curve of the line
-        a, b, c - Curve params
+        p1, 2, 3- Curve params
 
     Curve options are:
         lin     - Linear
         par     - Parabolic
         npar    - Negative Parabolic
-        cos     - Half-period Cosine (a = slope)
-        sinh    - Hyperbolic Sinusoidal (a = slope)
-        tanh    - Hyperbolic Tangent (a = slope)
-        exp     - Exponential (a = slope)
-        plin    - Periodic Linear (a = amp, b = peak)
-        ppar    - Periodic Parabolic (a = amp, b = peak, c = mode)
+        cos     - Half-period Cosine (p1 = slope)
+        sinh    - Hyperbolic Sinusoidal (p1 = slope)
+        tanh    - Hyperbolic Tangent (p1 = slope)
+        exp     - Exponential (p1 = slope)
+        plin    - Periodic Linear (p1 = amp, p2 = peak)
+        ppar    - Periodic Parabolic (p1 = amp, p2 = peak, p3 = mode)
                     Mode options: 0 = ++, 1 = +-, 2 = --, 3 = -+
-        psin    - Periodic Sinusoidal (a = amp (+ and -), b = slope, c = freq)
-        pcos    - Periodic Cosine (a = amp (+ only), b = slope c = freq)
+        psin    - Periodic Sinusoidal (p1 = amp (+ and -), p2 = slope, p3 = freq)
+        pcos    - Periodic Cosine (p1 = amp (+ only), p2 = slope p3 = freq)
     """
+    if curve in ['plin', 'pcos']:
+        if p2 <= 0 or p2 >= 1:
+            raise ValueError('peak setting out of bounds (must be')
+
     m = float(n)
     tmp = numpy.zeros(n, numpy.float32)
 
@@ -51,55 +55,48 @@ def crange(x, y, n, curve='lin', a=1.0, b=0.5, c=1.0):
     elif curve=='cos':
         d = y-x
         for i in xrange(n):
-            tmp[i] = (x+d*((math.cos(math.pi + (i/m)*math.pi))+1)/2)**a
+            tmp[i] = (x+d*((math.cos(math.pi + (i/m)*math.pi))+1)/2)**p1
     elif curve=='sinh':
         d = y-x
         tmp[0] = x
         for i in xrange(1,n):
-            tmp[i] = x+((math.sinh(a*(2*i-m)/m)-math.sinh(-a))/
-                        (2*math.sinh(a*(2*n-m)/m)/d))
+            tmp[i] = x+((math.sinh(p1*(2*i-m)/m)-math.sinh(-p1))/
+                        (2*math.sinh(p1*(2*n-m)/m)/d))
     elif curve=='tanh':
         d = y-x
         tmp[0] = x
         for i in xrange(1,n):
-            tmp[i] = x+((math.tanh(a*(2*i-m)/m)-math.tanh(-a))/
-                        (2*math.tanh(a*(2*n-m)/m)/d))
+            tmp[i] = x+((math.tanh(p1*(2*i-m)/m)-math.tanh(-p1))/
+                        (2*math.tanh(p1*(2*n-m)/m)/d))
     elif curve=='exp':
         d = y-x
         for i in xrange(n):
-            tmp[i] = d*(1-math.exp(-a*(i/m)))/(1-math.exp(-a))
+            tmp[i] = d*(1-math.exp(-p1*(i/m)))/(1-math.exp(-p1))
     elif curve=='plin':
-        n1=int(b*n)
-        n2=n-n1
-        mod = list(crange(0,a,n1,curve='lin'))
-        mod.extend(list(crange(a,0,n2,curve='lin')))
-        tmp = crange(x,y,n,curve='lin')
-        for i in xrange(n):
-            tmp[i] += mod[i]
+        np=int(p2*n)
+        tmp[:np] = crange(0,p1,np,curve='lin')
+        tmp[np:] = crange(p1,0,n-np,curve='lin')
+        tmp += crange(x,y,n,curve='lin')
     elif curve=='ppar':
-        n1=int(b*n)
-        n2=n-n1
-        mod = []
-        tmp = crange(x,y,n,curve='lin')
-        if c in [0, 1]: #+
-            mod.extend(list(crange(0,a,n1,curve='par')))
+        np=int(p2*n)
+        if p3 in [0, 1]: #+
+            tmp[:np] = crange(0,p1,np,curve='par')
         else:
-            mod.extend(list(crange(0,a,n1,curve='npar')))
-        if c in [0, 3]: #+
-            mod.extend(list(crange(a,0,n2,curve='par')))
+            tmp[:np] = crange(0,p1,np,curve='npar')
+        if p3 in [0, 3]: #+
+            tmp[np:] = crange(p1,0,n-np,curve='par')
         else:
-            mod.extend(list(crange(a,0,n2,curve='npar')))
-        for i in xrange(n):
-            tmp[i] += mod[i]
+            tmp[np:] = crange(p1,0,n-np,curve='npar')
+        tmp += crange(x,y,n,curve='lin')
     elif curve=='psin':
-        tmp = crange(x,y,n,curve='lin')
         for i in xrange(n):
-            tmp[i] += a*math.sin((i/m)*math.pi*2*c)#**b
+            tmp[i] = p1*math.sin((i/m)*math.pi*2*p3)#**b
+        tmp += crange(x,y,n,curve='lin')
     elif curve=='pcos':
         d = y-x
-        tmp = crange(x,y,n,'lin')
         for i in xrange(n):
-            tmp[i] += (a*((math.cos(math.pi + (i/m)*math.pi*2*c))+1)/2)**b
+            tmp[i] = (p1*((math.cos(math.pi + (i/m)*math.pi*2*p3))+1)/2)**p2
+        tmp += crange(x,y,n,'lin')
     else:
         return ValueError('Bad curve type')
     return tmp
@@ -124,7 +121,10 @@ class CP(object):
     def _get_spline(self):
         return self.t, self.c, self.b
 
-    spline = property(_get_spline, doc="Spline parameters in a list")
+    def _set_spline(self, params):
+        self.t, self.c, self.b = params
+
+    spline = property(_get_spline, _set_spline, doc="Spline parameters in a list")
 #---end CP
 
 def get_pad(target, neighbor, n):
@@ -151,7 +151,7 @@ def get_pad(target, neighbor, n):
         return CP(val)
 #---end get_pad
 
-def get_spline(my_cps, n=50, loop=False, curve='lin', a=1, b=0.5, c=1):
+def get_spline(my_cps, n=50, loop=False, curve='lin', p1=1, p2=0.5, p3=1):
     """Takes list CPs and returns full spline.
 
     Arguments:
@@ -166,60 +166,49 @@ def get_spline(my_cps, n=50, loop=False, curve='lin', a=1, b=0.5, c=1):
     #Segments is the number of interps to do, 1 less than #cps when not looping
     if loop: seg = len(my_cps)
     else:    seg = len(my_cps)-1
+    nf = seg * n
 
-    cps = my_cps[:]
-
-    #examples with [0, 1, 2, 3] - remember n-1, n, n+1, n+2 for spline
-    #  looping - (3,) 0, 1, 2, 3, (0, 1) (not interped, just used for spline)
-    #  not - (3,) 0, 1, 2, 3, (0) (not interped)
-    if loop:
-        cps.append(my_cps[0])
-        cps.append(my_cps[1])
-        cps.insert(0, my_cps[-1])
-    else:
-        cps.append(get_pad(my_cps[-2], my_cps[-1], -n))
-        cps.insert(0, get_pad(my_cps[0], my_cps[1], n))
-
-    #move vals into list (change columns to rows)
-    vals = []
-    if type(cps[0].val)==int or type(cps[0].val)==float:
-        #1D data
-        for c in cps:
-            vals.append(c.val)
+    if type(my_cps[0].val)==int or type(cps[0])==float:
         count = 1
     else:
-        #nD data
-        for i in xrange(len(cps[0].val)):
-            tmp = []
-            for c in cps:
-                tmp.append(c.val[i])
-            vals.append(tmp)
         count = len(cps[0].val)
 
-    tmp = []
+    vals = numpy.zeros((seg+3, count), numpy.float32)
+
+    #examples with [0, 1, 2, 3] - remember n-1, n, n+1, n+2 for spline
+    #  looping - (3,) 0, 1, 2, 3, 0, (1) (not interped, just used for spline)
+    #  not - (pad0,) 0, 1, 2, 3, (pad3) (not interped)
+    if loop:
+        vals[0] = my_cps[-1].val
+        for i in xrange(len(my_cps)):
+            vals[i+1] = my_cps[i].val
+        vals[-2] = my_cps[0].val
+        vals[-1] = my_cps[1].val
+        my_cps.append(my_cps[1])
+        my_cps.append(my_cps[2])
+        my_cps.insert(0, my_cps[-1])
+    else:
+        pad1 = get_pad(my_cps[0], my_cps[1], n)
+        pad2 = get_pad(my_cps[-2], my_cps[-1], -n)
+        vals[0] = pad1.val
+        for i in xrange(len(my_cps)):
+            vals[i+1] = my_cps[i].val
+        vals[-1] = pad2.val
+        my_cps.insert(0, pad1)
+        my_cps.append(pad2)
+
+    tmp = numpy.zeros((count, nf), numpy.float32)
     #do segments
     for i in xrange(seg):
-        tcps = cps[i:i+4]
-
-        if count==1:
-            #1D
-            tmp.extend(spline([cp.val for cp in tcps], n, tcps[1].spline,
-                       tcps[2].spline, curve=curve, a=a, b=b, c=c))
-        else:
-            #ND
-            #This code is ripe for improvement
-            ttmp = []
-            for i in xrange(count):
-                ttmp.append(spline([cp.val[i] for cp in tcps], n, tcps[1].spline,
-                            tcps[2].spline, curve=curve, a=a, b=b, c=c))
-            reord = []
-            for i in xrange(len(ttmp[0])):
-                row = []
-                for j in xrange(count):
-                    row.append(ttmp[j][i])
-                reord.append(row)
-            tmp.extend(reord)
-            #end improve section
+        print 'Segment: %d,' % i
+        tcps = my_cps[i:i+4]
+        i0, i1 = i*n, (i+1)*n
+        for j in xrange(count):
+            sp_tmp = spline(vals[i:i+4], n, tcps[1].spline, tcps[2].spline,
+                            curve=curve, p1=p1, p2=p2, p3=p3)
+            sp_tmp = sp_tmp.transpose()
+            tmp[j][i0:i1] = sp_tmp[j]
+        print
     #---end segments
     return tmp
 #---end get_spline
@@ -236,7 +225,7 @@ def spline(cps, n, splinea=(0, -1, 0), splineb=(0, -1, 0), **kwargs):
     """
     if len(cps) < 4: raise ValueError('Need 4 cps')
 
-    v = crange(0, 1, n, **kwargs)
+    t = crange(0, 1, n, **kwargs)
 
     ta, ca, ba = splinea
     tb, cb, bb = splineb
@@ -248,11 +237,11 @@ def spline(cps, n, splinea=(0, -1, 0), splineb=(0, -1, 0), **kwargs):
     M = numpy.array([[-fa,4+fa-fb-fc,-4+fb+fc-fd,fd]
                     ,[2*fa,-6-2*fa+2*fb+fc,6-2*fb-fc+fd,-fd]
                     ,[-fa,fa-fb,fb,0]
-                    ,[0,2,0,0]])/2.0
-    C = numpy.array(cps)
+                    ,[0,2,0,0]], numpy.float32)/2.0
+    C = numpy.array(cps, numpy.float32)
     MxC = numpy.dot(M,C)
-    S = []
-    for t in v:
-        S.append([t**3, t**2, t, 1])
-    return list(numpy.dot(numpy.array(S), MxC))
+    S = numpy.zeros((len(t),4), numpy.float32)
+    for i in xrange(len(t)):
+        S[i] = [t[i]**3, t[i]**2, t[i], 1]
+    return numpy.dot(S, MxC)
 #---end spline

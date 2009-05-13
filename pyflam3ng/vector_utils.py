8f5bc5a07c828a77afae9e1ca9ca29644b7c1e86
macbony <john.jj.miller@gmail.com>
2009-05-13 03:21:06 -0400

    Improve Spline and Vect

--- pyflam3ng/vector_utils.py
+++ pyflam3ng/vector_utils.py
@@ -103,6 +103,7 @@ class Vect(object):
     curve = property(_get_curve, _set_curve)
 
     def __len__(self):
+        self._length = self.end.time - self.start.time
         return self._length
 
     def _get_start(self):
@@ -122,13 +123,13 @@ class Vect(object):
         self.update()
 
     end = property(_get_end, _set_end)
+#---end Vect
 
 class Spline():
     def __init__(self, cps=None, loop=False, time=50):
         if cps==None:
             self._cps = []
             self._vects = []
-            self._length = 0
             self._count = 0
             self._looping = loop
             self._nframes = time
@@ -140,38 +141,21 @@ class Spline():
                 self._cps = get_cps_from_list(cps, time)
             self._looping = loop
             if len(self._cps)>1:
-                self._vects = self.setup_vects()
-                if self._looping:
-                    self._length = self._cps[-1].time + time
-                else:
-                    self._length = self._cps[-1].time
+                self.setup_vects()
             self._count = self._cps[0].val.size
 
     def set_all_splines(self, t, c, b):
         for cp in self._cps:
             cp.spline = t, c, b
 
-    def set_spline(self, index, t, c, b):
-        if index < len(self._cps):
-            cp[index].spline = t, c, b
-        else:
-            raise ValueError('index oob')
-
     def set_all_curves(self, curve, amp=0, freq=1, slope=1, peak=0.5, mode=0):
         if curve in valid_curves:
             for v in self._vects[1:-1]:
                 v.curve = curve, amp, freq, slope, peak, mode
-        else:
-            raise ValueError('invalid curve type')
 
     def set_curve(self, curve, amp=0, freq=1, slope=1, peak=0.5, mode=0):
-        if 0 < index < len(self._vects)-1:
-            if curve in valid_curves:
-                v[index].curve = curve, amp, freq, slope, peak, mode
-            else:
-                raise ValueError('invalid curve type')
-        elif index==0 or index==len(self._vects)-1:
-            raise ValueError('Edge vectors curves not used')
+        if 0 <= index < len(self._vects)-2:
+            v[index+1].curve = curve, amp, freq, slope, peak, mode
         else:
             raise ValueError('index oob')
 
@@ -190,33 +174,7 @@ class Spline():
         self._cps.insert(index, cp)
         for cp in self._cps[index+1:]:
             cp.time += dt
-        self._length = self._cps[-1].time
-        #check 5 different possibles
-        if self._looping and index==0:
-            #if it's a loop and first position, last segment loops to new 0
-            self._vects.insert(index, Vect(self._cps[-1], self._cps[index]))
-            self._vects[1].start = self._cps[index]
-            self._vects[-1].end = self._cps[index]
-        elif self._looping and index==len(self._cps):
-            #if it's the last position and a loop, last segment start = new
-            self._vects.append(Vect(self._cps[-1], self._cps[0]))
-            self._vects[0].start = self._cps[-1]
-            self._vects[-2].end = self._cps[-1]
-        elif index==0:
-            #if it's first in a flat, needs new pad
-            pad = self._cps[0].get_pad(self._cps[1])
-            self._vects.insert(index, Vect(pad, self._cps[0]))
-            self._vects[1].start = self._cps[0]
-        elif index==len(self._cps):
-            #if it's end in a flat, needs new pad
-            pad = self._cps[-1].get_pad(self._cps[-2])
-            self._vects.append(Vect(self._cps[-1], pad))
-            self._vects[-2].end = self._cps[-1]
-        else:
-            #middle
-            self._vects.insert(index, Vect(self._cps[index], self._cps[index+1]))
-            self._vects[index-1].end = self._cps[index]
-            self._vects[index+1].start = self._cps[index]
+        self.setup_vects()
     #---end
 
     def del_cp(self, index):
@@ -224,43 +182,20 @@ class Spline():
         if not 0 <= index < len(self._cps):
             raise ValueError('index oob')
         #remove cp at index and update times
-        if index==0: dt = self._cps[index+1].time - self._cps[index].time
-        else:        dt = self._cps[index].time - self._cps[index-1].time
+        if index < len(self._cps)-1:
+            dt = self._cps[index+1].time - self._cps[index].time
+        else:
+            dt = self._nframes
         self._cps.pop(index)
         for cp in self._cps[index+1:]:
             cp.time -= dt
-        self._length = self._cps[-1].time
-        #check 5 possibles
-        if self._looping and index==0:
-            #if loop and 0, last ends at new 0
-            self._vects.pop(0)
-            self._vects[0].start = self._cps[-1]
-            self._vects[-1].end = self._cps[0]
-        elif self._looping and index==len(self._cps):
-            #if loop and last, front starts at new last
-            self._vects = self._vects[:-1]
-            self._vects[0].start = self._cps[-1]
-            self._vects[-1].end = self._cps[0]
-        elif index==0:
-            #if flat and 0, need new pad for start
-            self._vects.pop(0)
-            pad = self._cps[0].get_pad(self._cps[1])
-            self._vects[0].start = pad
-        elif index==len(self._cps):
-            #if flat and last, last needs new pad for end
-            self._vects = self.vects[:-1]
-            pad = self._cps[-1].get_pad(self._cps[-2])
-            self._vects[-1].end = pad
-        else:
-            #middle
-            self._vects.pop(index)
-            self._vects[index-1].end = self._cps[index]
-            self._vects[index].start = self._cps[index]
+        self.setup_vects()
     #---end
 
     def calculate(self):
         if len(self._cps) < 2:
             raise ValueError('need more cps first')
+        self.setup_vects()
         tmp = numpy.zeros((self._count, self._length), numpy.float32)
         for i in xrange(1, len(self._vects)-1):
             i0 = self._vects[i].start.time
@@ -312,12 +247,21 @@ class Spline():
         tmp = []
         for i in xrange(seg+2):
             tmp.append(Vect(cps[i], cps[i+1]))
-        return tmp
+        self._vects = tmp
 
-    def _get_vects(self):
-        return self._vects
+    def resize_vect(self, index, length):
+        if index < 1 or index > len(self._vects)-1:
+            raise ValueError('Not a valid vector for resize')
+        diff = length - len(self._vects[index])
+        for i in xrange(index, len(self._vects)):
+            self._vects[i].end.time += diff
 
-    vects = property(_get_vects)
+    def loops(self):
+        return self._looping
+
+    def set_looping(self, loop):
+        self._looping = loop
+        self.setup_vects()
 
     def _get_cps(self):
         return self._cps
@@ -327,97 +271,30 @@ class Spline():
             self._cps = cps
         else:
             self._cps = get_cps_from_list(cps)
-        self.update()
+        self.setup_vects()
 
     cps = property(_get_cps, _set_cps)
 
+    def _get_nframes(self):
+        return self._nframes
+
+    def _set_nframes(self, time):
+        if type(time) <> int or time < 1:
+            raise ValueError('time must be positive int')
+        self._nframes = time
+        self.setup_vects()
+
+    nframes = property(_get_nframes, _set_nframes)
+
     def __len__(self):
+        if len(self._cps) < 2: return 0
+        self._length = self._vects[-2].end.time
         return self._length
+#---end Spline
+
 
 def get_cps_from_list(lst, time=50):
     tmp = []
     for i in xrange(len(lst)):
         tmp.append(CP(lst[i], i*time))
     return tmp
-
-def get_spline(my_cps, n=50, loop=False, curve='lin', amp=0, freq=1, slope=1,
-               peak=0.5, mode=0):
-    """Takes list CPs and returns full spline.
-
-    Arguments:
-        my_cps  - List of CP objects. All vals must have same dimensions.
-        loop    - Does the animation loop?
-        curve   - Curve shape. This will eventually be done per-segment.
-        amp, freq, mode,  - Curve params.
-    """
-    if len(my_cps)<2:
-        raise ValueError('Need 2 or more CPs')
-
-    #Segments is the number of interps to do, 1 less than #cps when not looping
-    if loop: seg = len(my_cps)
-    else:    seg = len(my_cps)-1
-
-    if type(my_cps[0].val)==int or type(my_cps[0])==float:
-        count = 1
-    else:
-        count = my_cps[0].val.size
-
-    vals = numpy.zeros((seg+3, count), dtype=numpy.float32)
-    times = numpy.zeros(seg+3, dtype=numpy.int32)
-
-    if   curve=='lin':  curve=-1
-    elif curve=='par':  curve=0
-    elif curve=='npar': curve=1
-    elif curve=='hcos': curve=2
-    elif curve=='sinh': curve=3
-    elif curve=='tanh': curve=4
-    elif curve=='exp':  curve=5
-    elif curve=='cos':  curve=6
-    elif curve=='sin':  curve=7
-    elif curve=='plin': curve=8
-    elif curve=='ppar': curve=9
-    else: raise ValueError('no such curve')
-
-    #examples with [0, 1, 2, 3] - remember n-1, n, n+1, n+2 for spline
-    #  looping - (3,) 0, 1, 2, 3, 0, (1) (not interped, just used for spline)
-    #  not - (pad0,) 0, 1, 2, 3, (pad3) (not interped)
-    if loop:
-        vals[0] = my_cps[-1].val
-        times[0] = my_cps[0].time-n
-        for i in xrange(len(my_cps)):
-            vals[i+1] = my_cps[i].val
-            times[i+1] = my_cps[i].time
-        vals[-2] = my_cps[0].val
-        times[-2] = my_cps[-3].time+n
-        vals[-1] = my_cps[1].val
-        times[-1] = my_cps[-3].time+2*n
-        my_cps.insert(0, my_cps[-1])
-        my_cps.append(my_cps[1])
-        my_cps.append(my_cps[2])
-    else:
-        pad1 = my_cps[0].get_pad(my_cps[1])
-        pad2 = my_cps[-1].get_pad(my_cps[-2])
-        vals[0] = pad1.val
-        times[0] = pad1.time
-        for i in xrange(len(my_cps)):
-            vals[i+1] = my_cps[i].val
-            times[i+1] = my_cps[i].val
-        vals[-1] = pad2.val
-        vals[-1] = pad2.time
-        my_cps.insert(0, pad1)
-        my_cps.append(pad2)
-    nf = times[-2]
-
-    tmp = numpy.zeros((count, nf), dtype=numpy.float32)
-    #do segments
-    for i in xrange(seg):
-        tcps = my_cps[i:i+4]
-        i0, i1 = i*n, (i+1)*n
-        for j in xrange(count):
-            ti, ci, bi, to, co, bo = tcps[1].spline_in, tcps[2].spline_out
-            sp_tmp = spline(vals[i:i+4,j], times[i:i+4], ti, ci, bi, to, co, bo,
-                            curve, amp, freq, slope, peak, mode)
-            tmp[j][i0:i1] = sp_tmp
-    #---end segments
-    return tmp
-#---end get_spline

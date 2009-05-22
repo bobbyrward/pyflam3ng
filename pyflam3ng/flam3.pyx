0dca89c21be20cb172adf47385131ec010affaf9
macbony <john.jj.miller@gmail.com>
2009-05-22 17:10:11 -0400

    Another crack at interpo = fail

--- pyflam3ng/flam3.pyx
+++ pyflam3ng/flam3.pyx
@@ -291,22 +291,23 @@ cdef class RenderBuffer:
         if self._buffer != NULL:
             stdlib.free(self._buffer)
             self._buffer = NULL
-
-def interpolate(list genome_list, int time):
-    cdef flam3_genome genomes
-    cdef flam3_genome result
+''' It overflows
+def interpolate(list genome_list, double time, double stagger=0.0):
+    cdef flam3_genome* genomes
+    cdef flam3_genome* res
+    cdef GenomeHandle result = GenomeHandle()
     cdef int ngenomes = len(genome_list)
 
-    genomes = <flam3_genome>_malloc(ngenomes * sizeof(flam3_genome))
-    result = <flam3_genome>_malloc(sizeof(flam3_genome))
+    genomes = <flam3_genome*>_malloc(ngenomes * sizeof(flam3_genome));
+    res = <flam3_genome*>_malloc(sizeof(flam3_genome));
 
     for idx, genome in enumerate(genome_list):
-        memmove(&genomes[idx], (<GenomeHandle>genome)._genome, sizeof(flam3_genome))
+        memmove(&genomes, (<GenomeHandle>genome)._genome, sizeof(flam3_genome))
 
-    flam3_interpolate(&genomes, ngenomes, time, 0.0, &result)
-    memmove((<GenomeHandle>result)._genome, &result, sizeof(flam3_genome))
+    flam3_interpolate(genomes, ngenomes, time, stagger, res)
+    memmove((<GenomeHandle>result)._genome, res, sizeof(flam3_genome))
     return result
-
+'''
 
 cdef int __render_callback(void *context, double progress, int stage, double eta) with gil:
     if context != NULL and (<object>context) is not None:
